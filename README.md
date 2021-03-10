f5c84bc051cb64f9ac5ab2d933d8ba307e03c2bd
nicher <niclas.hertzberg@gmail.com>
2021-03-10 20:39:33 +0100

    update

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
 # LT2222 V21 Assignment 2
 
-Assignment 2 from the course LT2222 in the University of Gothenburg's winter 2021 semester.
+Assignment 2 from the course LT2222 in the University of Gothenburg's SPRING 2021 semester.
 
-Your name: *here*
+Your name: Niclas Hertzberg
 
 *Answer all questions in the notebook here.  You should also write whatever high-level documentation you feel you need to here.*
+
+
+
+1. 
+For the preprocessing step I lemmatized each word and turned it into lowercase. I removed some punctuation but no other words since I did not find that it made the model predict better.
+
+2. 
+I made a "window" of words surrounding each instance. The window size is by reduced only by punctuation but is then extended by start and stop symbols so that each feature vector is of length 10.
+This makes sense since we only get 10 available slots of information around each word, it would be a good idea to actually use all of these slots and not ignore that valuable information. 
+Although a shorter sentence might actually be informative as well since then the length of each vector becomes a feature as well.
+I tested using a a sentence with several stop and start symbols and it seems to get slightly better results on the testing data.
+
+3.
+For the table creation I did a bag of words model. I tested it with TF-IDF but the result was not better, perhaps because each instance is the same length. 
+I added dimensionality reduction since the data is very sparse and it made the model generalize slightly better to new data, the model tended to overfit more to the testing data without dimensionality reduction (it performed very well on the training data but less well on the testing data).
+
+5.
+The diagonal across the matrix represent the cases where the prediction by the model were the same as the actual label, ie the correct prediction. Any values not in the diagonal represent an incorrect prediction.
+
+I chose to return the number of predictions instead of percentages since there was a big difference between how frequently occuring some entities were in the text and the number of entities were strongly correlated with how many correct predictions were made.
+
+Since the model is trained on the training data it will generally perform very well on the training data.
+Without dimensionality reduction the model will guess almost perfectly since it has been trained on that data.
+It seems like the model perhaps performed too well since it got almost everything right, and thus would not necessarily generalize well over to new data.
+To rectify this, I reduced dimensionality to 500 dimensions, which seemed to get the best result on the testing data, which is the only data used for evaluating the model. So although the model decreased its general accuracy on the training data, it is still preferable since the accuracy on the testing data improved slightly. 
+
+
+Bonus A:
+The model guessed wrong on all event entities and all natural phenomenon entities.
+It probably has to do with the fact that they were so uncommon compared to other types. 
+
+There were five classes in general that tended to be mistaken for each other, are also the most common classes, geo, gpe, org, per, tim.
+Regarding mistakes made because of linguistic factors, geographical entities and geopolitical entities were mistaken for each other quite frequently.
+An example of this could perhaps be geo: iran, gpe: iranian. While these are different words, the sentencens in which these words occur might be very similar.
+For example, two sentences like "the iranian nuclear deal have been criticized by the US government" or "the nuclear deal in iran have been criticized by the US government" would have the same surrounding context words.
+
+
+Bonus B:
+For bonus part b I added each words pos tag to that word, for example: "the-dt" instead of "the".
+In this way the vocabulary will be larger, and two words that have the same form might be diffentiated between each other by its tag, should it be different. 
+
+The model performed around 4% better by incorporating POS-tags.
+
+
--- a2.py
+++ a2.py
@@ -4,6 +4,13 @@ import numpy as np
 import numpy.random as npr
 import pandas as pd
 import random
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('wordnet')
+from collections import deque 
+from itertools import chain
+import matplotlib.pyplot as pyplot
+
 
 # Module file with functions that you fill in so that they can be
 # called by the notebook.  This file should be in the same
@@ -16,7 +23,31 @@ import random
 
 # Function for Part 1
 def preprocess(inputfile):
-    return inputfile.readlines()
+    text = inputfile.readlines()
+    lemmatizer = WordNetLemmatizer()
+
+    listoflines = []
+    for line in text:
+        row = nltk.word_tokenize(line)
+        listoflines.append(row)
+    
+    
+      #punctuation, not including dots 
+    punctuation = ("`", '?', '``', "？", '﹖', "'", '"', ",", "!", ";", "(", ")", "-", ";", ":", ">", "<", "@", "?", ":", '*', "-", "´", "`")
+    
+    
+    listofwords = []
+    for item in listoflines:
+        looplist = []
+        for word in item:
+            word = word.lower()
+            word = lemmatizer.lemmatize(word)
+            looplist.append(word)
+        
+        if item[2] not in punctuation:
+            listofwords.append(looplist)
+    
+    return listofwords
 
 # Code for part 2
 class Instance:
@@ -32,39 +63,212 @@ class Instance:
 
 def create_instances(data):
     instances = []
-    for i in range(100):
-        neclass = random.choice(['art','eve','geo','gpe','nat','org','per','tim'])
-        features = ["something"] * random.randint(1, 10)
-        instances.append(Instance(neclass, features))
+    wordlist = [x[2] for x in data]
+    before = []
+    after = []
+    
+    
+    before_words = [[a,b,c,d,e] for a,b,c,d,e in zip(wordlist,wordlist[1:],wordlist[2:],wordlist[3:],wordlist[4:])]
+    list_from_five = [[a,b,c] for a,b,c in zip(data[5:], data[6:], data[7:])]
+    after_words = [[a,b,c,d,e,f,g] for a,b,c,d,e,f,g in zip(wordlist[6:],wordlist[7:],wordlist[8:],wordlist[9:],wordlist[10:],wordlist[11:],wordlist[12:])]
+    
+    zipper = zip(before_words, list_from_five, after_words)
+    
+    startsymbols = [ "<S1>", "<S2>", "<S3>", "<S4>", "<S5>"]
+    endsymbols = ["</S1>", "</S2>", "</S3>", "</S4>", "</S5>"]
+    
+    
+    for before, middle, after in zipper:
+ 
+        if middle[0][4].startswith("b"):
+            if middle[1][4].startswith("i"):
+                after = after[1:]
+            elif not middle[1][4].startswith("i"):
+                after = after[:-1]
+            
+            if middle[2][4].startswith("i"):
+                after = after[1:]  
+            elif not middle[2][4].startswith("i"):
+                after = after[:-1]
+            
+            if "." in before:
+                before = before[before.index(".")+1:]
+            if "." in after:
+                after = after[:after.index(".")]
+            for i in range(len(endsymbols)):
+                if len(after) < 5:
+                    after.insert(len(after), endsymbols[i])
+            for i in range(len(endsymbols)):
+                if len(before) < 5:
+                    before.insert(i, startsymbols[i]) 
+            
+            allitems = []
+            neclass = middle[0][4][2:]
+            allitems.extend(before)
+            allitems.extend(after)
+            features = allitems
+            instances.append(Instance(neclass, features))
+    
+    
     return instances
+
+
+def reduce(matrix, dims=300):
+    from sklearn.decomposition import TruncatedSVD
+    sentence_names = matrix["classname"]
+    matrix = matrix.drop(labels="classname", axis=1)
+    
+    tsvd = TruncatedSVD(n_components=dims)
+    matrix = tsvd.fit_transform(matrix)
+    
+    dataframe = pd.DataFrame.from_records(matrix)
+    dataframe.insert(0,"classname",sentence_names)
+    return dataframe
+
+
+
 
 # Code for part 3
 def create_table(instances):
-    df = pd.DataFrame()
-    df['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(100)]
-    for i in range(3000):
-        df[i] = npr.random(100)
-
+    
+    wordlist = {}
+    for eachlist in instances:
+        for word in eachlist.features:
+            if word not in wordlist:
+                wordlist[word] = 1
+            wordlist[word] += 1
+    
+    topwords_to_columns = sorted(wordlist, key=wordlist.get, reverse=True)
+    
+    listofsents = []
+    listofclasses = []
+    for eachlist in instances:
+            one_sentence = {}
+            for word in wordlist.keys():
+                if word not in eachlist.features:
+                    one_sentence[word] = 0
+                else:
+                    wordcount = eachlist.features.count(word)
+                    one_sentence[word] = wordcount
+            listofsents.append(one_sentence)
+            listofclasses.append(eachlist.neclass)
+    
+    
+    
+    df = pd.DataFrame(data = listofsents, columns=topwords_to_columns)
+    df.insert(0,"classname",listofclasses)
+    
+    
+    df = reduce(df, dims=500)
+    
     return df
 
-def ttsplit(bigdf):
-    df_train = pd.DataFrame()
-    df_train['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(80)]
-    for i in range(3000):
-        df_train[i] = npr.random(80)
 
-    df_test = pd.DataFrame()
-    df_test['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(20)]
-    for i in range(3000):
-        df_test[i] = npr.random(20)
-        
-    return df_train.drop('class', axis=1).to_numpy(), df_train['class'], df_test.drop('class', axis=1).to_numpy(), df_test['class']
+def ttsplit(bigdf):
+    
+    dfgroups = bigdf.groupby("classname", group_keys=False)
+    df_test = dfgroups.apply(lambda x: x.sample(frac=0.2))
+    df_train = bigdf.drop(df_test.index, axis=0)
+    
+   # df_train = df_train.reset_index(drop=True)
+   # df_test = df_test.reset_index(drop=True)
+
+    
+    return (df_train.drop('classname', axis=1).to_numpy(), 
+           df_train['classname'], 
+           df_test.drop('classname', axis=1).to_numpy(),
+           df_test['classname'])
+    
 
 # Code for part 5
 def confusion_matrix(truth, predictions):
-    print("I'm confusing.")
-    return "I'm confused."
+    from sklearn.metrics import confusion_matrix
+    
+    cols = sorted(truth.unique())
+    conmat =  confusion_matrix(truth, predictions, labels=cols)
+    df = pd.DataFrame(conmat, index=cols, columns=cols)
+    
+    return df
 
-# Code for bonus part B
+
+
+
+#this function does the same thing as create_instances except for adding pos-tags to each word
+def create_instances2(data):
+    instances = []
+    wordlist = [str(x[2]) + "-" + str(x[3]) for x in data]
+    before = []
+    after = []
+    
+    poslist = [x[3] for x in data]
+    
+    before_pos = [[a,b,c,d,e] for a,b,c,d,e in zip(poslist,poslist[1:],poslist[2:],poslist[3:],poslist[4:])]
+    after_pos = [[a,b,c,d,e,f,g] for a,b,c,d,e,f,g in zip(poslist[6:],poslist[7:],poslist[8:],poslist[9:],poslist[10:],poslist[11:],poslist[12:])]
+    
+    before_words = [[a,b,c,d,e] for a,b,c,d,e in zip(wordlist,wordlist[1:],wordlist[2:],wordlist[3:],wordlist[4:])]
+    list_from_five = [[a,b,c] for a,b,c in zip(data[5:], data[6:], data[7:])]
+    after_words = [[a,b,c,d,e,f,g] for a,b,c,d,e,f,g in zip(wordlist[6:],wordlist[7:],wordlist[8:],wordlist[9:],wordlist[10:],wordlist[11:],wordlist[12:])]
+    
+    
+    
+    zipper = zip(before_words, list_from_five, after_words)
+    
+    startsymbols = [ "<S1>", "<S2>", "<S3>", "<S4>", "<S5>"]
+    endsymbols = ["</S1>", "</S2>", "</S3>", "</S4>", "</S5>"]
+    
+    
+    for before, middle, after in zipper:
+         
+        if middle[0][4].startswith("b"):
+            if middle[1][4].startswith("i"):
+                after = after[1:]
+            elif not middle[1][4].startswith("i"):
+                after = after[:-1]
+            
+            if middle[2][4].startswith("i"):
+                after = after[1:]
+            elif not middle[2][4].startswith("i"):
+                after = after[:-1]
+            
+            if '.-.' in before:
+                before = before[before.index('.-.')+1:]
+            if '.-.' in after:
+                after = after[:after.index('.-.')]
+                
+            
+            
+            for i in range(len(endsymbols)):
+                if len(after) < 5:
+                    after.insert(len(after), endsymbols[i])
+            for i in range(len(endsymbols)):
+                if len(before) < 5:
+                    before.insert(i, startsymbols[i])
+            
+            
+            allitems = []
+            neclass = middle[0][4][2:]
+            allitems.extend(before)
+            allitems.extend(after)
+            features = allitems
+            instances.append(Instance(neclass, features))
+    
+    
+    return instances
+
+
 def bonusb(filename):
-    pass
+    from sklearn.svm import LinearSVC
+    filename = open(filename, "r")
+    file = preprocess(filename)
+    filename.close()
+    instances = create_instances2(file)
+    bigdf = create_table(instances)
+    train_X, train_y, test_X, test_y = ttsplit(bigdf)
+    model = LinearSVC()
+    model.fit(train_X, train_y)
+    train_predictions = model.predict(train_X)
+    test_predictions = model.predict(test_X)
+    conmat = confusion_matrix(test_y, test_predictions)
+    conmat2 = confusion_matrix(train_y, train_predictions)
+    return print("test", conmat),print("train", conmat2)
+
